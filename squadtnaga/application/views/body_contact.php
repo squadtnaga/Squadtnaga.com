7a22fe2539e391c66cfa3184189ae200742ffded
faisalrmadjid <44545048+faisalrmadjid@users.noreply.github.com>
2018-10-29 00:25:08 +0700

    Update0.03
    -Buat halaman kontak
    -form belum lengkap

--- squadtnaga/application/views/body_contact.php
+++ squadtnaga/application/views/body_contact.php
@@ -1,39 +1,40 @@
 <div id="contactus">
 	<div class="row">
 		<div class="col-sm-7">
-			<h3>KOMENTAR</h3>
+			<h3>Contact Us</h3>
 			<div class="panel panel-default">
 				<div class="panel-body">
 					<form action="#">
 						<div class="form-group">
-							<label for="nama">Nama</label>
-							<input class="form-control" type="text" id="nama" name="nama">
+							<label for="Nama">Nama Perusahaan</label>
+							<input class="form-control" type="text" id="Nama" name="v" required>
 						</div>
 						<div class="form-group">
-							<label for="email">Email</label>
-							<input class="form-control" type="email" id="email" name="email">
+							<label for="Motto">Motto Perusahaan</label>
+							<input class="form-control" type="text" id="Motto" name="Motto" required>
 						</div>
 						<div class="form-group">
-							<label for="pesan">Isi Pesan</label>
-							<textarea id="pesan" class="form-control" name="pesan"></textarea>
+							<label for="Email">Email</label>
+							<input class="form-control" type="Email" id="Email" name="Email" required>
 						</div>
+						<div class="form-group">
+							<label for="Alamat">Alamat Perusahaan</label>
+							<textarea id="Alamat" class="form-control" name="Alamat" required></textarea>
+						</div>
+						<div class="form-group">
+							<label for="Deskripsi">Deskripsi Perusahaan anda</label>
+							<textarea id="Deskripsi" class="form-control" name="Deskripsi" placeholder="Kami adalah perusahaan yang bergerak di bidang...." required></textarea>
+						</div>
+						<div class="form-group">
+							<label for="Kontak">Kontak perusahaan yang dapat dihubungi(no telp, email, sosmed)?</label>
+							<input class="form-control" type="text" id="Kontak" name="Kontak" placeholder="085824810, ig:@abc, fb:abc, twitter:@abc " required>
+						</div>
+						
 						<input type="submit" class="btn btn-block btn-default" value="KIRIM">
 					</form>
 				</div>
 			</div>
 		</div>
-		<div class="col-sm-5">
-			<h3>ALAMAT</h3>
-			<br>
-			<h3>Kampus 1</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget urna magna.</p>
-			<h3>Kampus 2</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget urna magna.</p>
-			<h3>Kampus 3</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget urna magna.</p>
-			<div class="well well-lg">
-				<h3>Maps</h3>
-			</div>
-		</div>
+		
 	</div>
 </div>
