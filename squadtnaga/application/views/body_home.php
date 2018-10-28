797fc1b5269da691654e34451f5208fb6335b930
squadtnaga <38159399+squadtnaga@users.noreply.github.com>
2018-10-28 17:06:21 +0700

    update
    update body_home

--- squadtnaga/application/views/body_home.php
+++ squadtnaga/application/views/body_home.php
@@ -1,119 +1,121 @@
 <body style="position:relative;">
-<div class="content">
+  <div class="content">
 
-  <section  style=" padding: 5px; background: linear-gradient(to bottom, rgba(75,90,102,0.9) 0%, rgba(5,20,30,0.1) 100%); background-color:#00b1f4 ; "  >
+    <section  style=" padding: 5px; background: linear-gradient(to bottom, rgba(75,90,102,0.9) 0%, rgba(5,20,30,0.1) 100%); background-color:#00b1f4 ; "  >
+         
+        <div id="myCarousel" class="carousel slide" data-ride="carousel" >
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+        <center>
+          <div class="carousel-inner" style="opacity:1;"  >
+            <div class="item active">
+              <img src="<?=base_url("/assets/image/logouinjkt2.png")?>" style="  max-width:400px; max-height:400px;" >
+            
+              <div class="carousel-caption" >
+                <h3>haseme</h3>
+                <p>haseme</p>
+              </div>
+            
+            </div>
+
+            <div class="item">
+              <img src="<?=base_url("/assets/image/naga.png"); ?>" style="max-width:400px; max-height:400px;" >
+            
+              <div class="carousel-caption">
+                  <h3>Chicago</h3>
+                  <p>vdahd ukjhfbuhunwhrbsfhk,ahiuvh qihvinfahka,h vaihndv jadhvinads auvhain </p>
+                </div>
+            
+            </div>
+
+            <div class="item">
+              <img src="<?=base_url("/assets/image/sqdtng3.png"); ?>" style="max-width:400px; max-height:400px;" >
+            
+              <div class="carousel-caption">
+                <h3>vajnoqjnh</h3>
+                <p>adhvbauyvuayhejhva
+                  fafbdaadah
+                  abdakjhaudyhuafdhbah</p>
+              </div>
+            
+            </div>
+            
+          </div>
+        </center>
+
+        </div>
+    </section>
+
    
-  
-      <div id="myCarousel" class="carousel slide" data-ride="carousel" >
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
+    <section class="about us" style=" height: 410px; opacity: 40%; color:white;padding:5px; background-image: linear-gradient( grey,black);">
+      <center>
+        <h1 style="margin-top:80px;"><strong>About Us</strong></h1>
+          <p  style=" text-align:center; max-width: 700px;"> Kami adalah tim yang bergerak di bidang IT development. Terdiri dari mahasiswa-mahasiswa jurusan Teknik Informatika di UIN Jakarta. Dengan anda mempercayakan projek anda pada kami, anda telah membantu anak bangsa untuk berkembang dan siap di dunia kerja</p>
+          <br>
+          <button class="btn btn-info btn-lg" href="#">Selengkapnya -></button>
+      </center>
 
-        <!-- Wrapper for slides -->
-        <center><div class="carousel-inner" style="opacity:1;"  >
-          <div class="item active">
-           <img src="<?=base_url("/assets/image/logouinjkt2.png")?>" style="  max-width:400px; max-height:400px;" >
-            <div class="carousel-caption" >
-              <h3>haseme</h3>
-              <p>haseme</p>
-            </div>
-          </div>
-
-          <div class="item">
-          <img src="<?=base_url("/assets/image/naga.png"); ?>" style="max-width:400px; max-height:400px;" >
-            <div class="carousel-caption">
-              <h3>Chicago</h3>
-              <p>vdahd ukjhfbuhunwhrbsfhk,ahiuvh qihvinfahka,h vaihndv jadhvinads auvhain </p>
-            </div>
-          </div>
-
-          <div class="item">
-           <img src="<?=base_url("/assets/image/sqdtng3.png"); ?>" style="max-width:400px; max-height:400px;" >
-            <div class="carousel-caption">
-              <h3>vajnoqjnh</h3>
-              <p>adhvbauyvuayhejhva
-              fafbdaadah
-              abdakjhaudyhuafdhbah</p>
-            </div>
-          </div>
-        </div></center>
-
-  <!-- Left and right controls -->
-          <a class="left carousel-control" href="#myCarousel" data-slide="prev" style="max-height:400px;">
-    <span class="glyphicon glyphicon-chevron-left"><</span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" data-slide="next"  style="max-height:400px;">
-    <span class="glyphicon glyphicon-chevron-right">></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-
-      </div>
+    </section>
 
 
-  
-  </section>
-
-
-  <section class="about us" style=" height: 410px; opacity: 40%; color:white;padding:5px; background-image: linear-gradient( grey,black);">
-    <center>
-      <h1 style="margin-top:80px;"><strong>About Us</strong></h1>
-        <p  style=" text-align:center; max-width: 700px;"> Kami adalah tim yang bergerak di bidang IT development. Terdiri dari mahasiswa-mahasiswa jurusan Teknik Informatika di UIN Jakarta. Dengan anda mempercayakan projek anda pada kami, anda telah membantu anak bangsa untuk berkembang dan siap di dunia kerja</p>
+    <section class="What we do" style="padding:50px; background-color:blue; height: 710px;" >
+      <center>
+        <h1><strong>What We Do?</strong></h1>
+      </center>
         <br>
-        <button class="btn btn-info btn-lg" href="#">Selengkapnya -></button>
-    </center>
+        <div class="col-sm-4"  >
+          <div class="table-responsive">
+            <table class="table table-bordered" border="2" width="300px">
+              <tr><th style="text-align:center;">Web Development</th></tr>
+              <tr><td style="text-align:center;"><p>Anda memiliki perusahaan? organisasi? komunitas? 
+              atau toko yang perlu dibuatkan websitenya? Kami siap membantu anda
+               dengan harga yang sangat kekeluargaan</p> </td></tr> 
+            </table>
+          </div>      
+        </div>
+        
+        <div class="col-sm-4"  >
+          <div class="table-responsive">
+            <table class="table table-bordered" border="2" width="300px">
+              <tr><th style="text-align:center;">Wordpress</th></tr>
+              <tr><td style="text-align:center;"><p>Anda memiliki perusahaan? organisasi? komunitas yang membutuhkan web profil? Kami siap membantu anda
+               dengan harga yang sangat kekeluargaan</p> </td></tr> 
+            </table>
+          </div>      
+        </div>
 
+        <div class="col-sm-4"  >
+          <div class="table-responsive">
+            <table class="table table-bordered" border="2" width="300px">
+              <tr><th style="text-align:center;">Desain Grafis</th></tr>
+              <tr><td style="text-align:center;"><p>Anda membutuhkan desain logo? desain web? Kami siap membantu anda
+               dengan harga yang sangat kekeluargaan</p> </td></tr> 
+            </table>
+          </div>      
+        </div>
+    </section>
 
-  </section>
+    <section class="Portofolio" style="padding:50px margin-top:50px background-color:black; ">
+      <center>
+        <h1><strong>Our Portofolio</strong></h1>
+      </center>
+      
+      <center>
+        <img alt="jawarajahit.com" target="_blank" href="Jawarahit.com" src="<?= base_url("assets/image/SS jawarajahit.png"); ?>" style="max-width:500px; max-height: 500px; ">
+         
+          <figcaption>
+            <a target="_blank" href="https://www.jawarajahit.com">jawarajahit.com</a>
+          </figcaption>
 
-  <section class="What we do" style="padding:50px; background-color:blue; height: 710px;" >
-    <center>
-      <h1><strong>What We Do?</strong></h1></center>
-      <br>
-      <div class="col-sm-4"  >
-        <div class="table-responsive">
-        <table class="table table-bordered" border="2" width="300px">
-          <tr><th style="text-align:center;">Web Development</th></tr>
-          <tr><td style="text-align:center;"><p>Anda memiliki perusahaan? organisasi? komunitas? 
-          atau toko yang perlu dibuatkan websitenya? Kami siap membantu anda
-           dengan harga yang sangat kekeluargaan</p> </td></tr> 
-        </table>
-        </div>      
-      </div>
-         <div class="col-sm-4"  >
-        <div class="table-responsive">
-        <table class="table table-bordered" border="2" width="300px">
-          <tr><th style="text-align:center;">Wordpress</th></tr>
-          <tr><td style="text-align:center;"><p>Anda memiliki perusahaan? organisasi? komunitas yang membutuhkan web profil? Kami siap membantu anda
-           dengan harga yang sangat kekeluargaan</p> </td></tr> 
-        </table>
-        </div>      
-      </div>
-         <div class="col-sm-4"  >
-        <div class="table-responsive">
-        <table class="table table-bordered" border="2" width="300px">
-          <tr><th style="text-align:center;">Desain Grafis</th></tr>
-          <tr><td style="text-align:center;"><p>Anda membutuhkan desain logo? desain web? Kami siap membantu anda
-           dengan harga yang sangat kekeluargaan</p> </td></tr> 
-        </table>
-        </div>      
-      </div>
-  </section>
+      </center>
+    </section>
 
-   <section class="Portofolio" style="padding:50px margin-top:50px background-color:black; ">
-   <br>
-    <center>
-      <h1><strong>Our Portofolio</strong></h1>
-    </center>
     
-    <center>
-      <img alt="jawarajahit.com" title="Jawarahit.com" href="Jawarahit.com" src="<?= base_url("assets/image/SS jawarajahit.png"); ?>" style="width:500px; height: 500px; ">
-    </center>
-     
-  </section>
-</div>
+  </div>
 </body>
