64a7cdaf9ff919968ca27886319aa9ad48b6e1ff
squadtnaga <38159399+squadtnaga@users.noreply.github.com>
2018-10-28 19:53:23 +0700

    Dipakein panel
    Dipakein bootstrap panel

--- squadtnaga/application/views/body_home.php
+++ squadtnaga/application/views/body_home.php
@@ -1,8 +1,10 @@
-<body style="position:relative;">
-  <div class="content">
-
-    <section  style=" padding: 5px; background: linear-gradient(to bottom, rgba(75,90,102,0.9) 0%, rgba(5,20,30,0.1) 100%); background-color:#00b1f4 ; "  >
-         
+<body >
+  <div class="container">
+  <div class="panel-group">
+  
+    <section class="panel panel-default"  style=" background: linear-gradient(to bottom, rgba(75,90,102,0.9) 0%, rgba(5,20,30,0.1) 100%); background-color:#00b1f4 ; "  >
+      
+      <div class="panel-body"> 
         <div id="myCarousel" class="carousel slide" data-ride="carousel" >
           <!-- Indicators -->
           <ol class="carousel-indicators">
@@ -50,72 +52,105 @@
         </center>
 
         </div>
+      </div>
     </section>
 
    
-    <section class="about us" style=" height: 410px; opacity: 40%; color:white;padding:5px; background-image: linear-gradient( grey,black);">
-      <center>
-        <h1 style="margin-top:80px;"><strong>About Us</strong></h1>
-          <p  style=" text-align:center; max-width: 700px;"> Kami adalah tim yang bergerak di bidang IT development. Terdiri dari mahasiswa-mahasiswa jurusan Teknik Informatika di UIN Jakarta. Dengan anda mempercayakan projek anda pada kami, anda telah membantu anak bangsa untuk berkembang dan siap di dunia kerja</p>
+    <section class="panel panel-default" style=" height: 410px; opacity: 40%; color:white; background-image: linear-gradient( grey,black);">
+      <div class="panel-body">
+        <center>
+          <h1 style="margin-top:80px;"><strong>About Us</strong></h1>
+            <p  style=" text-align:center; max-width: 700px;"> Kami adalah tim yang bergerak di bidang IT development. Terdiri dari mahasiswa-mahasiswa jurusan Teknik Informatika di UIN Jakarta. Dengan anda mempercayakan projek anda pada kami, anda telah membantu anak bangsa untuk berkembang dan siap di dunia kerja</p>
+            <br>
+            <button class="btn btn-info btn-lg" href="#">Selengkapnya -></button>
+        </center>
+      </div>
+    </section>
+
+
+    <section class="panel panel-default" style="height: 710px;" >
+      <div class="panel-body">
+        <center>
+          <h1><strong>What We Do?</strong></h1>
+        </center>
           <br>
-          <button class="btn btn-info btn-lg" href="#">Selengkapnya -></button>
-      </center>
+          <div class="col-sm-4"  >
+            <div class="table-responsive">
+              <table class="table table-bordered" border="2" width="300px">
+                <tr><th style="text-align:center;">Web Development</th></tr>
+                <tr><td style="text-align:center;"><p>Anda memiliki perusahaan? organisasi? komunitas? 
+                atau toko yang perlu dibuatkan websitenya? Kami siap membantu anda
+                 dengan harga yang sangat kekeluargaan</p> </td></tr> 
+              </table>
+            </div>      
+          </div>
+          
+          <div class="col-sm-4"  >
+            <div class="table-responsive">
+              <table class="table table-bordered" border="2" width="300px">
+                <tr><th style="text-align:center;">Wordpress</th></tr>
+                <tr><td style="text-align:center;"><p>Anda memiliki perusahaan? organisasi? komunitas yang membutuhkan web profil? Kami siap membantu anda
+                 dengan harga yang sangat kekeluargaan</p> </td></tr> 
+              </table>
+            </div>      
+          </div>
 
-    </section>
+          <div class="col-sm-4"  >
+            <div class="table-responsive">
+              <table class="table table-bordered" border="2" width="300px">
+                <tr><th style="text-align:center;">Desain Grafis</th></tr>
+                <tr><td style="text-align:center;"><p>Anda membutuhkan desain logo? desain web? Kami siap membantu anda
+                 dengan harga yang sangat kekeluargaan</p> </td></tr> 
+              </table>
+            </div>      
+          </div>
+      </section>
 
-
-    <section class="What we do" style="padding:50px; background-color:blue; height: 710px;" >
-      <center>
-        <h1><strong>What We Do?</strong></h1>
-      </center>
-        <br>
-        <div class="col-sm-4"  >
-          <div class="table-responsive">
-            <table class="table table-bordered" border="2" width="300px">
-              <tr><th style="text-align:center;">Web Development</th></tr>
-              <tr><td style="text-align:center;"><p>Anda memiliki perusahaan? organisasi? komunitas? 
-              atau toko yang perlu dibuatkan websitenya? Kami siap membantu anda
-               dengan harga yang sangat kekeluargaan</p> </td></tr> 
-            </table>
-          </div>      
-        </div>
-        
-        <div class="col-sm-4"  >
-          <div class="table-responsive">
-            <table class="table table-bordered" border="2" width="300px">
-              <tr><th style="text-align:center;">Wordpress</th></tr>
-              <tr><td style="text-align:center;"><p>Anda memiliki perusahaan? organisasi? komunitas yang membutuhkan web profil? Kami siap membantu anda
-               dengan harga yang sangat kekeluargaan</p> </td></tr> 
-            </table>
-          </div>      
-        </div>
-
-        <div class="col-sm-4"  >
-          <div class="table-responsive">
-            <table class="table table-bordered" border="2" width="300px">
-              <tr><th style="text-align:center;">Desain Grafis</th></tr>
-              <tr><td style="text-align:center;"><p>Anda membutuhkan desain logo? desain web? Kami siap membantu anda
-               dengan harga yang sangat kekeluargaan</p> </td></tr> 
-            </table>
-          </div>      
-        </div>
-    </section>
-
-    <section class="Portofolio" style="padding:50px margin-top:50px background-color:black; ">
-      <center>
-        <h1><strong>Our Portofolio</strong></h1>
-      </center>
-      
-      <center>
-        <img alt="jawarajahit.com" target="_blank" href="Jawarahit.com" src="<?= base_url("assets/image/SS jawarajahit.png"); ?>" style="max-width:500px; max-height: 500px; ">
+      <section class="panel panel-body" style=" background-color:#ff3333; ">
+        <center>
+          <h1 style="color:white;"><strong>Our Portofolio</strong></h1>
+       
+            <div class="row">
+              <div class="col-md-4">
+                <div class="thumbnail">
+                  <a href="/w3images/lights.jpg" target="_blank">
+                    <img class="img-responsive" src="/w3images/lights.jpg" alt="Lights" style="width:100%">
+                    <div class="caption">
+                      <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            
+              <div class="col-md-4">
+                <div class="thumbnail">
+                  <a href="<?= base_url("assets/image/SS jawarajahit.png");?>" target="_blank">
+                    <img class="img-responsive" alt="jawarajahit.com" target="_blank" href="Jawarahit.com" src="<?= base_url("assets/image/SS jawarajahit.png"); ?>" style="max-width:500px; max-height: 500px; ">      
+                    <div class="caption">
+                     <a class="btn btn-success" target="_blank" href="https://www.jawarajahit.com">Kunjungi jawarajahit.com</a>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              
+              <div class="col-md-4">
+                <div class="thumbnail">
+                  <a href="/w3images/fjords.jpg" target="_blank">
+                    <img class="img-responsive" src="/w3images/fjords.jpg" alt="Fjords" style="width:100%">
+                    <div class="caption">
+                      <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              
+            </div>
          
-          <figcaption>
-            <a target="_blank" href="https://www.jawarajahit.com">jawarajahit.com</a>
-          </figcaption>
-
-      </center>
+       
+          <button class="btn btn-info btn-lg">Lihat Selengkapnya -></button>
+        </center>
+      </div>
     </section>
 
-    
-  </div>
-</body>
+
+  
